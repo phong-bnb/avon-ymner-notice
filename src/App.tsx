53f7a483e56ac65f4c4f9dee7d1a991a0053f7fe
Gia Phong <ngphong.7800@gmail.com>
2023-11-28 10:33:18 +0700

    bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import Bottom1 from './component/bottom/bottom'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+
+      <Bottom1/>
+    </div>
   )
 }
 
--- src/component/bottom/bottom.tsx
+++ src/component/bottom/bottom.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
-const bottom = () => {
+import "./bottom.scss";
+const Bottom1 = () => {
   return (
-    <div>bottom</div>
-  )
-}
+    <div className="bottom">
+      <div className="bottom__bottom2">
+        <img src="src/assets/img/logo.png" alt="logo" />
 
-export default bottom
+        <div className="bottom__bottom2__table">
+          <div className="bottom__bottom2__table__div">
+            <p>Available funding</p>
+            <p>Funding insights</p>
+          </div>
+          <div className="bottom__bottom2__table__div">
+            <p>About Ymner</p>
+            <p>About FinSight</p>
+          </div>
+          <div className="bottom__bottom2__table__div">
+            <p>The databasse</p>
+            <p>Cookies & GDPR</p>
+            <p>Contact: hello@ymner.com</p>
+          </div>
+        </div>
+      </div>
+      <div className="bottom__bottom1">
+        <div className="bottom__bottom1__left">
+          <img alt="co nuoc anh" src="src/assets/img/flag.png" />
+          <p>English</p>
+        </div>
+        <div className="bottom__bottom1__right">
+          <p>
+            {" "}
+            © COPYRIGHT THE YMNER SOCIETY AB 2020. ALLA RÄTTIGHETER FÖRBEHÅLLNA.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bottom1;
